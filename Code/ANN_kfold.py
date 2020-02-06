22bc2bf2982f11dac7970197ca5c0bcaa29f4987
Xin <58825067+xhuang2016@users.noreply.github.com>
2020-02-06 16:28:05 -0500

    Add files via upload

---
+++ Code/ANN_kfold.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+__author__ = 'Xin Huang, Chul-Ho Lee'
+"""
+====================================
+k-fold Cross Validation for ANN
+====================================
+"""
+import pandas as pd
+import numpy as np
+import math
+import os
+import glob
+from sklearn.model_selection import KFold
+from sklearn.cluster import KMeans
+from sklearn import preprocessing
+from sklearn.neural_network import MLPClassifier
+
+#=========================== Process EQ data ===================================
+work_path = 'earthquake'
+write_path = 'result1/'
+
+SamplingRate = 25 # need to be changed, 25/50/100
+Duration = 2 # need to be changed, 2/4/10
+
+original_SamplingRate = 100
+WindowSize = 2 * SamplingRate
+rate = original_SamplingRate/SamplingRate
+
+qry = work_path + '/*.txt'
+files = glob.glob(qry)
+n_events = len(files)
+EQ_features = []
+for fn in files:
+    # filename = os.path.splitext(os.path.split(fn)[1])[0]
+    data = pd.read_csv(fn, sep=' ',names=['X','Y','Z'])
+    data = data.iloc[0::int(rate)]
+    data = data.reset_index(drop=True)
+    X = data['X']
+    X_peak = np.where(X == np.max(X))[0][0] # find the peak
+    start = X_peak - int(SamplingRate)
+    end = X_peak + int(SamplingRate) * (Duration - 1)
+    df = data[start:end]
+    df = df.reset_index(drop=True)
+
+    X_tg = df['X']
+    Y_tg = df['Y']
+    Z_tg = df['Z']
+
+    VS = pow(pow(X_tg, 2) + pow(Y_tg, 2) + pow(Z_tg, 2), 1/2)
+
+    # 2 sec sliding window with 1 sec overlap
+    for j in np.arange(0, len(X)-SamplingRate, SamplingRate):
+        train = VS[j:j+WindowSize]
+        if len(train) == WindowSize:
+            #IQRs
+            Q75, Q25 = np.percentile(train, [75 ,25])
+            IQR = Q75 - Q25
+            #ZCs
+            ZCx=0
+            ZCy=0
+            ZCz=0
+            #CAVs
+            CAV=0
+            for i in range(j,j+WindowSize-1):
+                if ((X_tg[i]<0) != (X_tg[i+1]<0)):
+                    ZCx += 1
+                if ((Y_tg[i]<0) != (Y_tg[i+1]<0)):
+                    ZCy += 1
+                if ((Z_tg[i]<0) != (Z_tg[i+1]<0)):
+                    ZCz += 1
+                amp = (VS[i] + VS[i+1]) / 2.0
+                CAV += amp/SamplingRate
+            ZC = max(ZCx,ZCy,ZCz)
+            EQ_feature = [IQR, ZC, CAV]
+            EQ_features.append(EQ_feature)
+EQ_features = np.reshape(EQ_features, (n_events, Duration-1, 3))
+
+#=========================== Process HumanActivity data ===================================
+work_path2 = 'human'
+HA_features = []
+for root, dirs, files in os.walk(work_path2):
+    for folders in dirs:
+        qry2 = work_path2 + '/'+ folders + '/*.csv'
+        files2 = glob.glob(qry2)
+        for fn2 in files2:
+            # filename2 = os.path.splitext(os.path.split(fn2)[1])[0]
+            data = pd.read_csv(fn2, header=0)
+            df = data.iloc[0::int(rate)]
+            df = df.reset_index(drop=True)
+
+            X = df['x']
+            Y = df['y']
+            Z = df['z']
+
+            X_tg = (X - np.mean(X))/9.80665
+            Y_tg = (Y - np.mean(Y))/9.80665
+            Z_tg = (Z - np.mean(Z))/9.80665
+
+            VS = pow(pow(X_tg, 2) + pow(Y_tg, 2) + pow(Z_tg, 2), 1/2)
+
+            # 2 sec sliding window with 1 sec overlap
+            for j in np.arange(0, len(X)-SamplingRate, SamplingRate):
+                train = VS[j:j+WindowSize]
+                if len(train) == WindowSize:
+                    #IQRs
+                    Q75, Q25 = np.percentile(train, [75 ,25])
+                    IQR = Q75 - Q25
+                    #ZCs
+                    ZCx=0
+                    ZCy=0
+                    ZCz=0
+                    #CAVs
+                    CAV=0
+                    for i in range(j,j+WindowSize-1):
+                        if ((X_tg[i]<0) != (X_tg[i+1]<0)):
+                            ZCx += 1
+                        if ((Y_tg[i]<0) != (Y_tg[i+1]<0)):
+                            ZCy += 1
+                        if ((Z_tg[i]<0) != (Z_tg[i+1]<0)):
+                            ZCz += 1
+                        amp = (VS[i] + VS[i+1]) / 2.0
+                        CAV += amp/SamplingRate
+                    ZC = max(ZCx,ZCy,ZCz)
+                    HA_feature = [IQR, ZC, CAV]
+                    HA_features.append(HA_feature)
+HA_features = np.reshape(HA_features, (len(HA_features), 1, 3))
+
+#=========================== k-fold CV ===================================
+kf = KFold(n_splits=10, shuffle=True, random_state=42)
+i = 0
+for (train_index, test_index), (train_index2, test_index2) in zip(kf.split(EQ_features), kf.split(HA_features)):
+    EQ_train, EQ_test = EQ_features[train_index], EQ_features[test_index]
+    HA_train, HA_test = HA_features[train_index2], HA_features[test_index2]
+
+    #=========================== k-means ===================================
+    EQ_train = np.reshape(EQ_train, (len(EQ_train)*(Duration-1), 3))
+    EQ_train_y = np.ones(len(EQ_train))
+
+    HA_train = np.reshape(HA_train, (len(HA_train), 3))
+
+    kmeans = KMeans(n_clusters=len(EQ_train), random_state=42).fit(HA_train)
+
+    HA_train_centroid = kmeans.cluster_centers_
+    HA_train_y = np.zeros(len(HA_train_centroid))
+
+    ANN_train_X = np.vstack((EQ_train, HA_train_centroid))
+    ANN_train_y = np.hstack((EQ_train_y, HA_train_y))
+
+
+    EQ_test = np.reshape(EQ_test, (len(EQ_test)*(Duration-1), 3))
+    EQ_test_y = np.ones(len(EQ_test))
+
+    HA_test = np.reshape(HA_test, (len(HA_test), 3))
+    HA_test_y = np.zeros(len(HA_test))
+
+    ANN_test_X = np.vstack((EQ_test, HA_test))
+    ANN_test_y = np.hstack((EQ_test_y, HA_test_y))
+
+    #=========================== ANN ===================================
+    ##feature scaling
+    min_max_scaler = preprocessing.MinMaxScaler()
+    ANN_train_X = min_max_scaler.fit_transform(ANN_train_X)
+    ANN_test_X = min_max_scaler.transform(ANN_test_X)
+
+    ## ANN Training
+    mlp = MLPClassifier(hidden_layer_sizes=(5,), activation='logistic', solver='sgd', alpha=0, max_iter=10000, random_state=42, learning_rate_init=0.2)
+    mlp.fit(ANN_train_X, ANN_train_y.ravel())
+    ## ANN Testing
+    y_prob = mlp.predict_proba(ANN_test_X)
+    ## save prediction probability
+    result = np.concatenate((ANN_test_y.reshape(-1, 1), y_prob),axis=1)
+    df_result = pd.DataFrame(result,columns=['labels','prob_0','prob_1'])
+    df_result.to_csv(write_path + 'ANN_%s'%SamplingRate + 'Hz_%s'%Duration +'s_pred_fold_%s.csv'% i,index=False)
+    i += 1
---
+++ Code/CRNN_kfold.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+"""
+====================================
+k-fold Cross Validation for CRNN
+====================================
+"""
+import pandas as pd
+import numpy as np
+import os
+import glob
+from sklearn.model_selection import KFold
+import tensorflow as tf
+
+# #=========================== Process EQ data ===================================
+## Location of EQ data
+work_path = 'EQ'
+
+## Location to save the result
+write_path = 'result/'
+
+###======Settings for different test cases======###
+SamplingRate = 25 # need to be changed, 25/50/100
+Duration = 2 # need to be changed, 2/4/10
+###============###
+
+WindowSize = 2 * SamplingRate
+original_SamplingRate = 100
+rate = original_SamplingRate/SamplingRate
+
+qry = work_path + '/*.txt'
+files = glob.glob(qry)
+
+X_EQ=[]
+Y_EQ=[]
+Z_EQ=[]
+for fn in files:
+    ## Load EQ data
+    data = pd.read_csv(fn, sep=' ',names=['X','Y','Z'])
+    ## Down-sampling
+    data = data.iloc[0::int(rate)]
+    data = data.reset_index(drop=True)
+    ## Find the peak of x-axis component
+    X = data['X']
+    X_peak = np.where(X == np.max(X))[0][0]
+    ## Select earthquake duration
+    start = X_peak - int(SamplingRate)
+    end = X_peak + int(SamplingRate) * (Duration - 1)
+    df = data[start:end]
+    df = df.reset_index(drop=True)
+
+    X_tg = df['X']
+    Y_tg = df['Y']
+    Z_tg = df['Z']
+
+    ## 2 sec sliding window with 1 sec overlap
+    for j in np.arange(0, len(X)-SamplingRate, SamplingRate):
+        X_batch = X_tg[j:j+WindowSize]
+        Y_batch = Y_tg[j:j+WindowSize]
+        Z_batch = Z_tg[j:j+WindowSize]
+        if len(X_batch) == WindowSize:
+            X_EQ.append(X_batch.values)
+            Y_EQ.append(Y_batch.values)
+            Z_EQ.append(Z_batch.values)
+
+X_EQ = np.asarray(X_EQ)
+Y_EQ = np.asarray(Y_EQ)
+Z_EQ = np.asarray(Z_EQ)
+
+X_EQ = X_EQ.reshape(X_EQ.shape[0],X_EQ.shape[1],1)
+Y_EQ = Y_EQ.reshape(Y_EQ.shape[0],Y_EQ.shape[1],1)
+Z_EQ = Z_EQ.reshape(Z_EQ.shape[0],Z_EQ.shape[1],1)
+
+# #=========================== Process Non-Earthquake data ===================================
+## Location of Non-Earthquake data
+work_path2 = 'NonEQ'
+
+X_HA=[]
+Y_HA=[]
+Z_HA=[]
+for root, dirs, files in os.walk(work_path2):
+    for folders in dirs:
+        qry2 = work_path2 + '/'+ folders + '/*.csv'
+        files2 = glob.glob(qry2)
+        for fn2 in files2:
+            ## Load Non-EQ data
+            data = pd.read_csv(fn2, header=0)
+            ## Down-sampling
+            df = data.iloc[0::int(rate)]
+            df = df.reset_index(drop=True)
+
+            X = df['x']
+            Y = df['y']
+            Z = df['z']
+
+            ## Move to ground by subtracting the mean, and convert [m/s^2] to [g]
+            X_tg = (X - np.mean(X))/9.80665
+            Y_tg = (Y - np.mean(Y))/9.80665
+            Z_tg = (Z - np.mean(Z))/9.80665
+
+            ## 2 sec sliding window with 1 sec overlap
+            for j in np.arange(0, len(X)-SamplingRate, SamplingRate):
+                X_batch = X_tg[j:j+WindowSize]
+                Y_batch = Y_tg[j:j+WindowSize]
+                Z_batch = Z_tg[j:j+WindowSize]
+                if len(X_batch) == WindowSize:
+                    X_HA.append(X_batch.values)
+                    Y_HA.append(Y_batch.values)
+                    Z_HA.append(Z_batch.values)
+
+X_HA = np.asarray(X_HA)
+Y_HA = np.asarray(Y_HA)
+Z_HA = np.asarray(Z_HA)
+
+X_HA = X_HA.reshape(X_HA.shape[0],X_HA.shape[1],1)
+Y_HA = Y_HA.reshape(Y_HA.shape[0],Y_HA.shape[1],1)
+Z_HA = Z_HA.reshape(Z_HA.shape[0],Z_HA.shape[1],1)
+
+
+# #=========================== k-fold Cross Validation ===================================
+kf = KFold(n_splits=10, shuffle=True, random_state=42)
+i = 0
+for (train_index, test_index), (train_index2, test_index2) in zip(kf.split(X_EQ), kf.split(X_HA)):
+
+    X_EQ_train, X_EQ_test = X_EQ[train_index], X_EQ[test_index]
+    Y_EQ_train, Y_EQ_test = Y_EQ[train_index], Y_EQ[test_index]
+    Z_EQ_train, Z_EQ_test = Z_EQ[train_index], Z_EQ[test_index]
+
+    X_HA_train, X_HA_test = X_HA[train_index2], X_HA[test_index2]
+    Y_HA_train, Y_HA_test = Y_HA[train_index2], Y_HA[test_index2]
+    Z_HA_train, Z_HA_test = Z_HA[train_index2], Z_HA[test_index2]
+
+    # #=========================== CRNN ===================================
+    ### CRNN Training
+    EQ_X_train = np.dstack((X_EQ_train, Y_EQ_train, Z_EQ_train))
+    HA_X_train = np.dstack((X_HA_train, Y_HA_train, Z_HA_train))
+
+    EQ_y_train = np.ones(len(X_EQ_train))
+    HA_y_train = np.zeros(len(X_HA_train))
+
+    X_train = np.vstack((EQ_X_train, HA_X_train))
+    y_train = np.hstack((EQ_y_train, HA_y_train)).reshape(-1,1)
+
+    ratio = float(len(HA_y_train)/len(EQ_y_train))
+    class_weights = {0: 1., 1: ratio}
+
+    verbose, epochs, batch_size = 0, 100, 256
+    n_features, n_outputs = X_train.shape[1], X_train.shape[2], y_train.shape[1]
+    n_steps, n_length = 2, SamplingRate
+
+    X_train = X_train.reshape((X_train.shape[0], n_steps, n_length, n_features))
+
+    model=tf.keras.models.Sequential()
+    model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Conv1D(filters=64, kernel_size=3, activation='relu'), input_shape=(None,n_length,n_features)))
+    model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Conv1D(filters=64, kernel_size=3, activation='relu')))
+    model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dropout(0.5)))
+    model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPooling1D(pool_size=2)))
+    model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten()))
+    model.add(tf.keras.layers.SimpleRNN(100))
+    model.add(tf.keras.layers.Dropout(0.5))
+    model.add(tf.keras.layers.Dense(100, activation='relu'))
+    model.add(tf.keras.layers.Dense(n_outputs, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, class_weight=class_weights, verbose=verbose)
+
+    ### CRNN Testing
+    EQ_X_test = np.dstack((X_EQ_test, Y_EQ_test, Z_EQ_test))
+    HA_X_test = np.dstack((X_HA_test, Y_HA_test, Z_HA_test))
+
+    EQ_y_test = np.ones(len(X_EQ_test))
+    HA_y_test = np.zeros(len(X_HA_test))
+
+    X_test = np.vstack((EQ_X_test, HA_X_test))
+    y_test = np.hstack((EQ_y_test, HA_y_test)).reshape(-1,1)
+
+    X_test = X_test.reshape((X_test.shape[0], n_steps, n_length, n_features))
+
+    y_prob = model.predict(X_test).ravel()
+
+    ## save ouput prediction probability
+    result = np.concatenate((y_test, y_prob.reshape(-1, 1)),axis=1)
+    df_result = pd.DataFrame(result,columns=['labels','prob_1'])
+    df_result.to_csv(write_path + 'CRNN_%s'%SamplingRate + 'Hz_%s'%Duration +'s_fold_%s.csv'% i,index=False)
+    i += 1
